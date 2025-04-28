32e31e41bb57ba38bc48f7ff4f8164b454bf94d7
Startang141 <bintangadiyatma141@gmail.com>
2025-04-28 10:36:26 +0700

    fix folder node modules

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,20 +1,38 @@
 import mongoose from "mongoose";
+import validator from "validator";
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
   name: {
     type: String,
     required: [true, "Name is required"],
+    unique: [true, "Name must be unique"],
   },
   email: {
     type: String,
     required: [true, "Email is required"],
+    unique: [true, "Email must be unique"],
+    validate: {
+      validator: validator.isEmail,
+      message: "Email is not valid",
+    },
   },
   password: {
     type: String,
     required: [true, "Password is required"],
+    minLength: [6, "Password must be at least 6 characters"],
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
   },
 });
+userSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 const User = mongoose.model("User", userSchema);
 
 export default User;
